99951cc772dba2be2b315ae1a3d9c6c688769005
Abdallah <abdallaahnoor@gmail.com>
2024-11-17 22:29:23 +0200

    refactor: 🔨 root Layout

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -2,9 +2,9 @@ import Navbar from "@/components/Navbar";
 
 export default function RootLayout({ children }) {
   return (
-    <main className="relative">
+    <div className="relative">
       <Navbar />
       {children}
-    </main>
+    </div>
   );
 }
