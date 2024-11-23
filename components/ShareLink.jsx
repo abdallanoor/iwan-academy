6d1cc5a3d5e30ec2361cdfb6536339ebd24ece37
Abdallah <abdallaahnoor@gmail.com>
2024-11-23 08:31:14 +0200

    style: 🎨 shareLink

--- components/ShareLink.jsx
+++ components/ShareLink.jsx
@@ -5,7 +5,7 @@ export default function ShareLink({ title, href, reverse, className, blank }) {
   return (
     <Link
       className={cn(
-        "uppercase max-sm:w-full text-[11px] text-center font-medium py-4 px-7 bg-primary text-white rounded-[2px] hover:bg-white hover:text-primary border border-primary transition-colors duration-300 leading-4 ltr:tracking-widest",
+        "uppercase inline-block text-[11px] text-center font-medium py-4 px-7 bg-primary text-white rounded-[2px] hover:bg-white hover:text-primary border border-primary transition-colors duration-300 leading-4 ltr:tracking-widest",
         reverse && "bg-white text-primary hover:bg-primary hover:text-white",
         className
       )}
