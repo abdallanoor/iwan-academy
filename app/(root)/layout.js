88b8bce163a934db78b869351930f408e08c7bf0
Abdallah <abdallaahnoor@gmail.com>
2024-11-23 09:16:45 +0200

    feat: ✨ add Book Free Evaluation link in layout

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -1,14 +1,26 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import ScrollToTop from "@/components/ScrollToTop";
+import ShareLink from "@/components/ShareLink";
+import { FREE_EVALUATION_URL } from "@/constants";
+import { useTranslations } from "next-intl";
 
 export default function RootLayout({ children }) {
+  const t = useTranslations("");
   return (
     <div className="relative">
       <Navbar />
       {children}
       <Footer />
       <ScrollToTop />
+      <div className="sm:hidden fixed bottom-5 left-1/2 -translate-x-1/2">
+        <ShareLink
+          title={t("bookFree")}
+          href={FREE_EVALUATION_URL}
+          className="ltr:tracking-normal w-max"
+          blank
+        />
+      </div>
     </div>
   );
 }
